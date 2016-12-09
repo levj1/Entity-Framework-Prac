c0b3e177c640390bfda2e32867f1f99c48f90a07
levj1 <levj1@yahoo.com>
2016-12-09 15:34:41 -0500

    Insert using entity framework
    Use Add and AddRange methods to insert rows to a database

---
+++ EF-Practise/EF.ConsoleApplication/Program.cs
@@ -0,0 +1,60 @@
+﻿using Data.Access;
+using Domain.Classes;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // database initialization work
+            Database.SetInitializer(new NullDatabaseInitializer<ContactContext>());
+            
+
+
+            Console.ReadLine();
+        }
+
+        private static void InsertContact()
+        {
+            var contact = new Contact
+            {
+                
+            };
+        }
+
+        private static void InsertAddress()
+        {
+            var address = new Address
+            {
+                AddressLine1 = "234 Orange Rd",
+                AddressLine2 = "apt #1a",
+                City = "Citywide",
+                State = "DC",
+                ZipCode = "12354"
+            };
+
+            var address1 = new Address
+            {
+                AddressLine1 = "54th Rd",
+                City = "Easwood",
+                State = "MD",
+                ZipCode = "54321"
+            };
+
+            using (var context = new ContactContext())
+            {
+                context.Database.Log = Console.WriteLine;
+                // use Add for one and AddRange for a list
+                context.Addresses.AddRange(new List<Address> { address, address1 });
+                context.SaveChanges();
+            }
+        }
+    }
+}

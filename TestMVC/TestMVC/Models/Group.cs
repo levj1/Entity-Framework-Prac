2d40232f5800c583170f1683f6460089e6d37e91
levj1 <levj1@yahoo.com>
2016-12-22 09:47:44 -0500

    idk

---
+++ TestMVC/TestMVC/Models/Group.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestMVC.Models
+{
+    public class Group
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime GroupDateCreated { get; set; }
+
+        public Group()
+        {
+            GroupDateCreated = DateTime.Now;
+        }
+    }
+}
